0e6311f217d32d65570ddc3868a339d34184babc
brian_ogola <brianochiengogola78@gmail.com>
2024-10-02 12:51:39 +0300

    Style error

--- src/components/contact/MapArea.tsx
+++ src/components/contact/MapArea.tsx
@@ -12,10 +12,10 @@ const MapArea = () => {
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3988.8454581117835!2d36.80237917363138!3d-1.26530599872265!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x182f173fc261c85d%3A0xc5db81b7b61f20ae!2sMadonna%20House%20Annex!5e0!3m2!1sen!2ske!4v1727862215835!5m2!1sen!2ske">
               width="1920"
               height="800"
-              style={{ border: 0 }}
+              {/* style={{ border: 0 }} */}
               allowFullScreen
               loading="lazy"
-            ></iframe>
+            </iframe>
           </div>
         </div>
       </div>
