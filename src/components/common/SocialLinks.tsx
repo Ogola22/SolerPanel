58e4b0acbd5e887ee8ce039c0be25122a78808c3
brian_ogola <brianochiengogola78@gmail.com>
2024-10-04 00:39:05 +0300

    Footer copy_right updated

--- src/components/common/SocialLinks.tsx
+++ src/components/common/SocialLinks.tsx
@@ -153,7 +153,7 @@ type copy_right_text_type = {
 
 const copy_right_text: copy_right_text_type = {
   copy_right: <>
-     ©{new Date().getFullYear()} | Alrights reserved by <a href="https://themeforest.net/user/theme_house" target="_blank"><br />Besca Engineering Co Limited</a>
+     ©{new Date().getFullYear()} | Alrights reserved <a href="#" target="_blank"><br />Besca Engineering Co Limited</a>
   </>
 }
 
