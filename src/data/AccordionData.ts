e74676748d42f2596bbfc3771f6316e88e7d7418
brian_ogola <brianochiengogola78@gmail.com>
2024-10-03 13:14:58 +0300

    Accordion updated

--- src/data/AccordionData.ts
+++ src/data/AccordionData.ts
@@ -16,20 +16,20 @@ const accordion_data = [
     question: 'How does solar energy help combat climate change?', 
     answer: "Solar energy reduces reliance on fossil fuels, which are major contributors to greenhouse gas emissions. By switching to solar, you help decrease carbon dioxide and other pollutants in the atmosphere.",
   },
-  {
-    id: "Four",
-    question: 'What is a Solar PV System?', 
-    answer: "A Solar Photovoltaic (PV) system converts sunlight directly into electricity using solar panels. The electricity can be used immediately or stored in batteries for later use.",
-  },
-  {
-    id: "Five",
-    question: 'Is solar energy a good investment for commercial buildings?', 
-    answer: "Yes, installing a Solar PV System can significantly reduce energy costs for commercial buildings. Over time, it leads to considerable savings on electricity bills and is a sustainable option for businesses.",
-  },,
-  {
-    id: "Six",
-    question: 'Can I completely power my home with solar energy?', 
-    answer: "Yes, with the right system size and battery storage, it’s possible to power your home entirely with solar energy. Besca Engineering will help determine the most efficient setup for your energy needs.",
-  }
+  // {
+  //   id: "Four",
+  //   question: 'What is a Solar PV System?', 
+  //   answer: "A Solar Photovoltaic (PV) system converts sunlight directly into electricity using solar panels. The electricity can be used immediately or stored in batteries for later use.",
+  // },
+  // {
+  //   id: "Five",
+  //   question: 'Is solar energy a good investment for commercial buildings?', 
+  //   answer: "Yes, installing a Solar PV System can significantly reduce energy costs for commercial buildings. Over time, it leads to considerable savings on electricity bills and is a sustainable option for businesses.",
+  // },,
+  // {
+  //   id: "Six",
+  //   question: 'Can I completely power my home with solar energy?', 
+  //   answer: "Yes, with the right system size and battery storage, it’s possible to power your home entirely with solar energy. Besca Engineering will help determine the most efficient setup for your energy needs.",
+  // }
 ]
 export default accordion_data
